a110fe8b82068a7c044c454a42c635649a51d362
Yurii Timkov <timkow11@gmail.com>
2020-11-28 15:47:40 +0200

    Initial commit

---
+++ db.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+)
+type Car struct{
+	Brand string
+	Country string
+	ProductionYear int
+	Price int
+}
+const (
+	DB_USER = "postgres"
+	DB_PASSWORD = "root"
+	DB_NAME = "cars"
+)
+func dbConnect() error {
+	var err error
+	db, err = sql.Open("postgres", fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
+	DB_USER, DB_PASSWORD, DB_NAME))
+	if err != nil {
+		return err
+	}
+	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS car (brand text, country text, production_year integer, price integer )"); err != nil {
+	return err
+}
+return nil
+}
+
+func dbAddCar(brand string, country string, production_year int, price int) error {
+	sqlstmt := "INSERT INTO car VALUES ($1, $2, $3, $4)"
+	_, err := db.Exec(sqlstmt, brand, country, production_year, price)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func dbGetCars() ([]Car, error) {
+	var cars []Car
+	stmt, err := db.Prepare("SELECT brand, country, production_year, price FROM car")
+	if err != nil {
+		return cars, err
+	}
+	res, err := stmt.Query()
+	if err != nil {
+		return cars, err
+	}
+	var tempCar Car
+	for res.Next() {
+		err = res.Scan(&tempCar.Brand, &tempCar.Country, &tempCar.ProductionYear, &tempCar.Price)
+		if err != nil {
+			return cars, err
+		}
+		cars = append(cars, tempCar)
+	}
+	return cars, err
+}
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+var db *sql.DB
+func rollHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		t, err := template.ParseFiles("simple_list.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		cars, err := dbGetCars()
+		if err != nil {
+			log.Fatal(err)
+		}
+		t.Execute(w, cars)
+	}
+}
+func addCarHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		t, err := template.ParseFiles("simple_form.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		brand := r.Form.Get("brand")
+		country:= r.Form.Get("country")
+		production_year, errI := strconv.Atoi(r.Form.Get("production_year"))
+		price, errI := strconv.Atoi(r.Form.Get("price"))
+		err := dbAddCar(brand, country, production_year, price)
+		if err != nil || errI != nil {
+			log.Fatal(err)
+		}
+	}
+}
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	if port == "" {
+		port = "4747"
+		fmt.Println(port)
+	}
+	return ":" + port
+}
+
+func main() {
+	err := dbConnect()
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.HandleFunc("/", rollHandler)
+	http.HandleFunc("/add", addCarHandler)
+	log.Fatal(http.ListenAndServe(GetPort(), nil))
+}
