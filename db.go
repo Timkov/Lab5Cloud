2bad7d0de648cf1c072b9ed5ae860f1da88f91be
Yurii Timkov <timkow11@gmail.com>
2020-11-28 17:31:19 +0200

    Try fix db connection

--- db.go
+++ db.go
@@ -17,7 +17,7 @@ const (
 )
 func dbConnect() error {
 	var err error
-	db, err = sql.Open("postgres", fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
+	db, err = sql.Open("postgres", fmt.Sprintf("user=%s password=%s dbname=%s sslmode=enable",
 	DB_USER, DB_PASSWORD, DB_NAME))
 	if err != nil {
 		return err
